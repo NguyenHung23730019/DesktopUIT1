f4e78e9ad2b861c9b3915c13f446c1b9211f7f9a
NguyenHung23730019 <149292975+NguyenHung23730019@users.noreply.github.com>
2023-11-15 10:27:25 +0700

    Update Bai1Lab1.java - hello github

--- Bai1Lab1.java
+++ Bai1Lab1.java
@@ -14,7 +14,7 @@ public class Bai1Lab1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("Hello github");
     }
     
 }
